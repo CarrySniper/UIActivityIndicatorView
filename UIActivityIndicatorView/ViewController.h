d93953439c9d2f148f5e34638091fff3ccd9fc3a
cjq002 <seejoys@sijiukejideMac-mini.local>
2016-06-03 16:08:02 +0800

    新建工程

---
+++ UIActivityIndicatorView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UIActivityIndicatorView
+//
+//  Created by 思久科技 on 16/6/3.
+//  Copyright © 2016年 Seejoys. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ UIActivityIndicatorView/ViewController.m
@@ -0,0 +1,54 @@
+//
+//  ViewController.m
+//  UIActivityIndicatorView
+//
+//  Created by 思久科技 on 16/6/3.
+//  Copyright © 2016年 Seejoys. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    //1.初始化
+    UIActivityIndicatorView *aiv = [[UIActivityIndicatorView alloc] init];
+    //2.设置位置，中心为self.view的中心。可以不设置frame，因为宽高是系统限制的。
+    aiv.center = self.view.center;
+    //3.添加到self.view上
+    [self.view addSubview:aiv];
+    
+    
+    //设置风格
+    /**
+     *  UIActivityIndicatorViewStyleWhiteLarge      白、大号
+     *  UIActivityIndicatorViewStyleWhite           白
+     *  UIActivityIndicatorViewStyleGray            灰
+     */
+    aiv.activityIndicatorViewStyle = UIActivityIndicatorViewStyleWhiteLarge;
+    //设置颜色
+    aiv.color = [UIColor redColor];
+    //设置停止时是否隐藏
+    aiv.hidesWhenStopped = YES;
+    
+    [aiv startAnimating];//开始动画
+//    [aiv stopAnimating];//停止动画
+    
+    //获取指示器动画状态
+    [aiv isAnimating];
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
